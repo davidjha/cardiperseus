7064c76ccf5885b1a7b1912d4716d8494eda3ec6
davidjha <davidjha21@gmail.com>
2020-05-03 13:26:12 -0400

    Removed record booleans. Compute number of games.

--- hearts_agents.py
+++ hearts_agents.py
@@ -1,22 +1,30 @@
 
 agent_1 = {
     'name': 'The Random Agent',
-    'losses': 0
+    'losses': 0,
+    'average': 0,
+    'games': 0
 }
 
 agent_2 = {
     'name': 'The LowLayer',
-    'losses': 0
+    'losses': 0,
+    'average': 0,
+    'games': 0
 }
 
 agent_3 = {
     'name': 'The Equalizer',
-    'losses': 0
+    'losses': 0,
+    'average': 0,
+    'games': 0
 }
 
 agent_4 = {
     'name': 'The Shooter',
-    'losses': 0
+    'losses': 0,
+    'average': 0,
+    'games': 0
 }
 
 hearts_agents = [agent_1, agent_2, agent_3, agent_4]
--- perseus.py
+++ perseus.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pprint
 from hearts_agents import hearts_agents
 
 df = pd.read_csv(r'de_randomized_hearts.csv')
@@ -16,6 +17,47 @@ def convert_to_int(score):
     return list_of_ints
 
 
+def find_agent(agent_list, index):
+    # agent_list = [agent1, agent2, agent3, agent4]
+    for i, agent in enumerate(agent_list):
+        if i == index:
+            return agent
+
+
+def find_average_points():
+    pass
+
+
+def compute_number_of_games(agent):
+    count = 0
+    for key, value in df.iterrows():
+        if value['agent1'] == agent:
+            count = count + 1
+        if value['agent2'] == agent:
+            count = count + 1
+        if value['agent3'] == agent:
+            count = count + 1
+        if value['agent4'] == agent:
+            count = count + 1
+
+        hearts_agents[agent - 1]['games'] = count
+    return count
+
+
+def number_of_games():
+    return len(df.index)
+
+
+def compute_losses():
+    for key, value in df.iterrows():
+        game_score = convert_to_int(value['score'])
+        max_score, max_index = get_max_score(game_score)
+        agent_list = [value['agent1'], value['agent2'], value['agent3'], value['agent4']]
+        worst_agent = find_agent(agent_list, max_index)
+        hearts_agents[worst_agent - 1]['losses'] += 1
+        # print(game_score, max_score, worst_agent, max_index)
+
+
 def get_max_score(score):
     """
     Returns the max score from a list of ints, and the index of this max score.
@@ -28,28 +70,11 @@ def get_max_score(score):
     return m_score, m_score_index
 
 
-def find_agent(agent_list, index, record_result):
-    # agent_list = [agent1, agent2, agent3, agent4]
-    for i, agent in enumerate(agent_list):
-        if i == index:
-            if record_result:
-                hearts_agents[i]['losses'] += 1
-            return agent
-
-
-def find_average_points():
-    pass
-
-
-def get_worst_player():
-    for key, value in df.iterrows():
-        game_score = convert_to_int(value['score'])
-        max_score, max_index = get_max_score(game_score)
-        # worst_agent = find_agent(value['agent1'], value['agent2'], value['agent3'], value['agent4'], max_index, True)
-        worst_agent = find_agent(df[['agent1', 'agent2', 'agent3', 'agent4']], max_index, True)
-        print(game_score, max_score, worst_agent, max_index)
-
-
-
-
-
+if __name__ == "__main__":
+    compute_losses()
+    agent1 = compute_number_of_games(1)
+    agent2 = compute_number_of_games(2)
+    agent3 = compute_number_of_games(3)
+    agent4 = compute_number_of_games(4)
+    print(number_of_games())
+    pprint.pprint(hearts_agents)
